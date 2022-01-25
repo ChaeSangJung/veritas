be079686650859e7ddf0de3062660118e1d18bc0
che <che@cheui-MacBookPro.local>
2022-01-25 17:05:40 +0900

    first commit

---
+++ veritas-1.ts
@@ -0,0 +1,40 @@
+// 액션 캑체들의 타입
+type CounterAction = ReturnType<typeof increase> | ReturnType<typeof decrease> | ReturnType<typeof increaseBy>
+
+// state 타입
+type CounterState = {
+    count : number;
+}
+
+interface Place {
+  name: string;
+  addr: string;
+}
+interface PlacesProps {
+  placeList: Place[];
+}
+
+interface IImg {
+    id: string;
+    urls : {
+        regular:string;
+    };
+    alt_description: string;
+}
+
+interface IImgProps {
+    loading:boolean;
+    data : IImg[];
+    error:string;
+    keyword:string;
+    totalPages:number;    
+}
+
+// 초기 상태 선언
+const initialState : IImgProps = {
+    loading: true,
+    data: [],
+    error: "",
+    keyword: "",
+    totalPages : 0
+}
